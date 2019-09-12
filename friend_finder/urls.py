542be26b286c1e05c9ab1c7e4b6b2132e711b207
dburtnja <dostup.te@gmail.com>
2019-09-12 18:34:51 +0300

    django project

--- friend_finder/urls.py
+++ friend_finder/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.json_test_request, name='test_request'),
+    path(r'', views.json_test_request, name='test_request'),
+    path(r"get_user/", views.get_user, name='get_user')
 ]
--- friend_finder/views.py
+++ friend_finder/views.py
@@ -2,10 +2,26 @@ from django.shortcuts import render
 import pyrebase
 from friend_finder.models import Place, User, Event
 from django.views.generic.base import TemplateView
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponseNotFound, HttpResponseNotAllowed
+import json
+from .models import User
 
 # Create your views here.
 
 
-def json_test_request(request):
-    return JsonResponse({"request body": "request.body"})
+def get_user(request):
+    if request.method != "GET":
+        return HttpResponseNotAllowed(["GET"])
+
+    id = request.GET.get("id")
+    users = User.objects.filter(id=id)
+
+    if len(users) == 0:
+        return HttpResponseNotFound()
+    user_data = users[0]
+
+    return JsonResponse({
+        "name": user_data.name,
+        "status": user_data.status,
+        "picture": user_data.image_url
+    })
