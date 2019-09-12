0d93696db5aa7b18c00c57883df3cf10b6c653a8
Vlastyslav <vlastyslav.kandyba@ericsson.com>
2019-09-12 17:32:15 +0300

    migration upd

--- friend_finder/admin.py
+++ friend_finder/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Event, Place, User
+
+admin.site.register(Event)
+admin.site.register(Place)
+admin.site.register(User)
 
 # Register your models here.
--- friend_finder/models.py
+++ friend_finder/models.py
@@ -4,8 +4,7 @@ from django.db import models
 class Place(models.Model):
     name = models.CharField(max_length=64, unique=True)
     description = models.CharField(max_length=256, blank=True, null=True)
-    loc_lat = models.FloatField()
-    loc_lon = models.FloatField()
+    location = models.CharField(max_length=64, unique=True, null=True)
     image_url = models.TextField(blank=True, null=True)
 
     def __str__(self):
@@ -13,12 +12,11 @@ class Place(models.Model):
 
 
 class User(models.Model):
+    nick = models.CharField(max_length=64, unique=True, null=True)
     name = models.CharField(max_length=64)
-    surname = models.CharField(max_length=64)
-    mail = models.CharField(max_length=64, unique=True)
+    mail = models.CharField(max_length=64, unique=True, blank=True, null=True)
     image_url = models.TextField(blank=True, null=True)
-    status = models.CharField(max_length=64)
-    id_token = models.TextField(blank=True, null=True)
+    status = models.CharField(max_length=64, null=True)
 
     def __str__(self):
         return self.name
@@ -27,10 +25,12 @@ class User(models.Model):
 class Event(models.Model):
     name = models.CharField(max_length=64, unique=True)
     place = models.ForeignKey(Place, on_delete=models.CASCADE)
-    creator = models.ForeignKey(User, on_delete=models.CASCADE)
+    creator = models.ForeignKey(User, on_delete=models.CASCADE,
+                                related_name='user_creator',
+                                null=True)
     timestamp = models.DateTimeField()
     note = models.CharField(max_length=256, blank=True, null=True)
-    people_amount = models.IntegerField(blank=True, null=True)
+    people_amount = models.ManyToManyField(User, blank=True)
     drinks_amount = models.IntegerField(blank=True, null=True)
 
     def __str__(self):
