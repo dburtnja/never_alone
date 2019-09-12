6cbf2ff6e736560566bd05682f6a277355ef6296
Vlastyslav <vlastyslav.kandyba@ericsson.com>
2019-09-12 13:22:50 +0300

    base models added to django app

--- friend_finder/models.py
+++ friend_finder/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.contrib.gis.db import models
 
-# Create your models here.
+
+class Place(models.Model):
+    name = models.CharField(max_length=64, unique=True)
+    description = models.CharField(max_length=256, blank=True, null=True)
+    loc_lat = models.FloatField()
+    loc_lon = models.FloatField()
+    image_url = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+
+class User(models.Model):
+    name = models.CharField(max_length=64, unique=True)
+    description = models.CharField(max_length=256, blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Event(models.Model):
+    name = models.CharField(max_length=64, unique=True)
+    place = models.ForeignKey(Place, on_delete=models.CASCADE)
+    user = models.ManyToManyField(User, on_delete=models.CASCADE())
+    timestamp = models.DateTimeField()
+    description = models.CharField(max_length=256, blank=True, null=True)
+
+    def __str__(self):
+        return self.name
